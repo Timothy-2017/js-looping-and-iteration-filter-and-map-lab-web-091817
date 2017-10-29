b7dcb04b7764677a53ef9208a32df8470608f07a
Timothy-2017 <timothy.eubank@gmail.com>
2017-10-29 14:09:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  const revenueDrivers = [];
+  drivers.filter(function (driver) {if (driver['revenue'] > revenue) {revenueDrivers.push(driver)}});
+  return revenueDrivers;
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  const revenueDrivers = [];
+  drivers.filter(function (driver) {if (driver['revenue'] > revenue) {revenueDrivers.push(driver['name'])}});
+  return revenueDrivers;
+}
+
+function exactMatch(drivers, obj) {
+  const matchDriversArr = [];
+  const key = Object.keys(obj)[0];
+  drivers.filter(function (driver) {if (driver[key] === obj[key]) {matchDriversArr.push(driver)}});
+  return matchDriversArr;
+}
+
+function exactMatchToList(drivers, obj) {
+  const matchDriversArr = [];
+  const key = Object.keys(obj)[0];
+  drivers.filter(function (driver) {if (driver[key] === obj[key]) {matchDriversArr.push(driver['name'])}});
+  return matchDriversArr;
+}
